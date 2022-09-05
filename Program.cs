3dac89d50588cbea39206b969d101f2cc0241db5
Olesia Shevtsova <ollcute.shevtsova@yandex.ru>
2022-09-05 13:02:01 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection.Emit;
+
+
+namespace GetIPCS
+{
+    /// 
+
+    /// Получаем IP адреса локальной машины
+    /// 
+
+    class classGetIPCS
+    {
+        /// 
+
+        /// Получаем IP адреса локальной машины
+        /// 
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Получаем имя компьютера
+            String strHostName = Dns.GetHostName();
+
+            // Ищем хост по имени
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Перечисляем IP адреса
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                ipaddress.ToString());
+            }
+            
+
+
+
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}.{1}     ",
+                    computerProperties.HostName, computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)//предостережение отсутствия сетевых интерфейсов
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return;
+            }
+
+            Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties(); //  .GetIPInterfaceProperties();
+                Console.WriteLine();
+                Console.WriteLine(adapter.Description);
+                Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
+                Console.Write("  Physical address ........................ : ");
+                PhysicalAddress address = adapter.GetPhysicalAddress();
+                byte[] bytes = address.GetAddressBytes();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    // Display the physical address in hexadecimal.
+                    Console.Write("{0}", bytes[i].ToString("X2"));
+                    // Insert a hyphen after each byte, unless we are at the end of the
+                    // address.
+                    if (i != bytes.Length - 1)
+                    {
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+
+    }  
+}
+
